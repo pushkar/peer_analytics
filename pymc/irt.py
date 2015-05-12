76cb50bd81030fc3e02266bbdcded7aaa4a08d6e
pushkar <pushkarkolhe@gmail.com>
2015-05-12 03:27:38 -0400

    Correct according to paper

--- pymc/irt.py
+++ pymc/irt.py
@@ -8,69 +8,75 @@ import scipy.stats as stats
 colors = ["#348ABD", "#A60628"]
 np.set_printoptions(precision=2)
 
-n_students = 1
-n_questions = 10
+n_students = 5
+n_questions = 5
 
 def gen_artificial_dataset(n_s, n_q):
     x = np.arange(0, n_q, 1)
     data = np.zeros(shape=(n_s, n_q), dtype=int)
+
     s_scores = np.r_[pm.rdiscrete_uniform(1, 100, n_s)/100.]
-    q_scores = np.r_[np.arange(0.1, 1.1, 1./n_q)]
+    q_scores = np.r_[pm.rdiscrete_uniform(1, 100, n_q)/100.]
+
+    #s_scores = np.r_[np.arange(0.1, 1.1, 1./n_s)]
+    #q_scores = np.r_[np.arange(0.1, 1.1, 1./n_q)]
+
     for si in range(0, n_s):
         for qi in range(0, n_q):
-            if s_scores[si] > q_scores[qi]:
+            if s_scores[si] >= q_scores[qi]:
                 data[si, qi] = 1
 
     return data, x, s_scores, q_scores
 
 data, x, s_scores, q_scores = gen_artificial_dataset(n_students, n_questions)
-
 print "Student Proficiency is " + str(s_scores)
-y = data[0,:]
-print x, y
+print "Question Proficiency is " + str(q_scores)
 
-proficiency = pm.Normal("prof", 0, 0.01, value = 0)
-hardness = pm.Normal("hardness", 0, 0.01, value = 0)
+s = np.empty(n_students, dtype=object)
+for i in range(0, n_students):
+    s[i] = pm.Normal('s_%i'%i, mu=0., tau=1.)
 
-@pm.deterministic
-def p(x=x, p=proficiency, h=hardness):
-    return 1.0/(1. + np.exp(p*x + h))
+q = np.empty(n_questions, dtype=object)
+for i in range(0, n_questions):
+    q[i] = pm.Normal('q_%i'%i, mu=0., tau=1.)
 
-observed = pm.Bernoulli("observed", p, value=y, observed=True)
+@pm.potential
+def p(data=data, s=s, q=q):
+    sum = 0
+    for i in range(1, n_students):
+        for j in range(1, n_questions):
+            r = data[i, j]
+            sum -= np.log(1.0 + np.exp(-s[i]+q[j]))
+            sum -= (1 - r)*(s[i]-q[j])
 
-model = pm.Model([proficiency, hardness, observed])
+    for i in range(1, n_students):
+        sum -= 0.5*s[i]**2
+
+    for j in range(1, n_questions):
+        sum -= 0.5*q[j]**2
+
+    return sum
+
+
+model = pm.Model(np.r_[s, q], p)
 map_ = pm.MAP(model)
 map_.fit()
 mcmc = pm.MCMC(model)
-mcmc.sample(12000, 4000, 2)
+mcmc.sample(50000, 18000)
 
-proficiency_samples = mcmc.trace("prof")[:, None]
-hardness_samples = mcmc.trace("hardness")[:, None]
+print "Student Proficiency:"
+for i in range(0, n_students):
+    print mcmc.stats()["s_%i"%i]["mean"]
 
-print mcmc.stats()["prof"]["mean"]
-print mcmc.stats()["hardness"]["mean"]
+print "Question Hardness:"
+for i in range(0, n_students):
+    print mcmc.stats()["q_%i"%i]["mean"]
 
-def logistic(x, beta, alpha=0):
-    return 1.0 / (1.0 + np.exp(np.dot(beta, x) + alpha))
-
-x_ = np.linspace(0, 10, 200)
-
-plt.subplot(311)
-plt.scatter(x, y, s=75, color="k", alpha=0.5)
-plt.yticks([0, 1])
-plt.plot(x_, logistic(x_, mcmc.stats()["prof"]["mean"], mcmc.stats()["hardness"]["mean"]), color="#348ABD")
-plt.ylabel("Prof")
-plt.xlabel("Questiom")
-plt.title("IRT")
-
-plt.subplot(312)
-plt.hist(proficiency_samples, histtype='stepfilled', bins=35, alpha=0.85,
-         label=r"posterior of prof", color="#7A68A6", normed=True)
-plt.legend()
-
-plt.subplot(313)
-plt.hist(hardness_samples, histtype='stepfilled', bins=35, alpha=0.85,
-         label=r"posterior of hard", color="#A60628", normed=True)
-plt.legend();
-
-show()
+data_n = np.zeros(shape=(n_students, n_questions), dtype=int)
+for si in range(0, n_students):
+    for qi in range(0, n_questions):
+        if mcmc.stats()["s_%i"%si]["mean"] >= mcmc.stats()["q_%i"%qi]["mean"]:
+            data_n[si, qi] = 1
+print data
+print data_n
+print logical_xor(data, data_n)
